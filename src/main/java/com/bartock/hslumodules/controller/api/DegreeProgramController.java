6676f444983df74aa3692f01fcf657db1305716f
Luca Bartock <luca.bartoeck@stud.hslu.ch>
2021-05-27 10:50:35 +0200

    REST Controllers: removed cross origin restriction

--- src/main/java/com/bartock/hslumodules/controller/api/DegreeProgramController.java
+++ src/main/java/com/bartock/hslumodules/controller/api/DegreeProgramController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin
 @RequestMapping("/api/degreeProgram")
 public class DegreeProgramController extends AbstractRestController {
 
